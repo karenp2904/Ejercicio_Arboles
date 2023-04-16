ef6f1079270cb9b3832cebc2f066806683f06e92
Karen <karen2005yulieth@gmail.com>
2023-04-15 21:58:01 -0500

    Metodo para añadir por posorden intento1

--- src/BinaryTree_Package/Main.java
+++ src/BinaryTree_Package/Main.java
@@ -13,11 +13,20 @@ public class Main {
         arbol.addWidth("buuu");
         arbol.addWidth("yupi");
 
-        System.out.println(arbol.posOrdenToString());
-        System.out.println(arbol.inordenToString());
+        //System.out.println(arbol.posOrdenToString());
+        //System.out.println(arbol.inordenToString());
+        //System.out.println(arbol.preOrdenToString());
+        //System.out.println(arbol.widthOrderToString());
+
+
+        System.out.println(arbol.remove("c"));
+        System.out.println(arbol.addDepth("karen"));
+        System.out.println(arbol.isComplete());
+        System.out.println(arbol.isFull());
         System.out.println(arbol.preOrdenToString());
 
-        System.out.println(arbol.widthOrderToString());
+
+
         //arbol.addWidth("holaaa");
 
         //System.out.println(arbol.root.left.getObjeto());
